4c7336ce9008fd5504f104cc1e57c734000d9d8c
Sumit <sumit@clearquote.io>
2024-04-07 08:30:08 +0530

    asdg

--- solutions/ML5/train_and_save_tc.py
+++ solutions/ML5/train_and_save_tc.py
@@ -2,6 +2,7 @@ import turicreate as tc
 
 tc.config.set_num_gpus(1)
 
+print("GPUs being used based on config: ", tc.config.get_num_gpus())
 
 # Load the starter images
 starter_images = tc.SFrame({'image':[tc.Image('wheelrim_testing/cropped_wheelrim.png')], 'label':['wheelrim']})
@@ -12,7 +13,7 @@ test_images = tc.SFrame({'image':[tc.Image('wheelrim_testing/frame_11910.jpg'),
 
 
 # Create a model. This step will take a few hours on CPU and about an hour on GPU
-model = tc.one_shot_object_detector.create(starter_images, 'label')
+model = tc.one_shot_object_detector.create(starter_images, 'label', max_iterations = 3)
 
 
 # Save predictions on the test set
@@ -25,6 +26,10 @@ test_images['annotated_predictions'] = \
     
     
 print("Predictions for the test images: \n", test_images['predictions'])
+
+print("\n\n")
+print("Annotated Predictions for the test images: \n", test_images['annotated_predictions'])
+print("\n\n")
 
 print("Saving model to disk ..")
 # Save the model for later use in TuriCreate
